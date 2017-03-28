04ef5b8d63db948a9385ef1d8264b12f0caae85c
m1nd <m1nd@mail.ru>
2017-03-28 20:11:58 +0300

    Add and configating Webpack Dev Server

--- src/server/index.js
+++ src/server/index.js
@@ -18,5 +18,6 @@ app.get('/', (req, res) => {
 })
 
 app.listen(WEB_PORT, () => {
-  console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' : '(development)'}.`)
+  console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' : 
+  '(development).\nKeep "yarn dev:wds" running in an other terminal'}.`)
 })
